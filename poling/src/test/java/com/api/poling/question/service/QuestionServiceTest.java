f7015f031c9f64e0cb446421a6f1441a2b7dc41d
Yunus Emre Diril <dirilyunusemread@outlook.com>
2022-03-15 13:40:40 +0300

    question service unit tests

--- poling/src/test/java/com/api/poling/question/service/QuestionServiceTest.java
+++ poling/src/test/java/com/api/poling/question/service/QuestionServiceTest.java
@@ -1,8 +1,13 @@
 package com.api.poling.question.service;
 
 import com.api.poling.auth.service.UserService;
+import com.api.poling.question.dao.Answer;
 import com.api.poling.question.dao.Question;
+import com.api.poling.question.dao.UserAnswer;
 import com.api.poling.question.dto.ApproveQuestionRequest;
+import com.api.poling.question.dto.RetrieveAnswerResponse;
+import com.api.poling.question.dto.ReviewUserAnswerRequest;
+import com.api.poling.question.dto.SaveAnswerRequest;
 import com.api.poling.question.enums.Status;
 import com.api.poling.question.exception.SourceNotFoundException;
 import com.api.poling.question.repository.QuestionRepository;
@@ -14,7 +19,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
+import java.util.*;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
@@ -40,6 +45,19 @@ class QuestionServiceTest {
 
     @Test
     void saveAnswer() {
+        SaveAnswerRequest request = SaveAnswerRequest.builder()
+                .optionNo(1)
+                .userId("u11")
+                .questionId("b12")
+                .build();
+        Question question = buildQuestion();
+        when(userService.findUserById(request.getUserId()))
+                .thenReturn(Optional.empty());
+        when(questionRepository.findById(request.getQuestionId()))
+                .thenReturn(Optional.of(question));
+
+        verify(questionRepository, times(1)).save(question);
+        assertThat(question.getAnswers().get(0).getAnswerCount()).isNotEqualTo(0);
     }
 
     @Test
@@ -48,6 +66,19 @@ class QuestionServiceTest {
 
     @Test
     void retrieveAnswer() {
+        ReviewUserAnswerRequest request = ReviewUserAnswerRequest.builder()
+                .questionId("b12")
+                .userId("u1")
+                .build();
+        when(userAnswerRepository.findByQuestionIdAndUserId(anyString(), anyString())).thenReturn(Optional.of(UserAnswer.builder()
+                .questionId("b12")
+                .userId("u1")
+                .optionNo(1)
+                .build()));
+
+        RetrieveAnswerResponse retrieveAnswerResponse = questionService.retrieveAnswer(request);
+        assertThat(retrieveAnswerResponse.getOptionNo()).isNotNull();
+        assertThat(retrieveAnswerResponse.getOptionNo()).isEqualTo(1);
     }
 
     @Test
@@ -101,5 +132,26 @@ class QuestionServiceTest {
 
     @Test
     void deleteQuestion() {
+        String questionId = "b12";
+        questionService.deleteQuestion("b12");
+        ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+        verify(questionRepository).deleteById(stringArgumentCaptor.capture());
+        String capturedQuestionId = stringArgumentCaptor.getValue();
+        assertThat(questionId).isEqualTo(capturedQuestionId);
+    }
+
+    Question buildQuestion() {
+        return Question.builder()
+                .answers(buildAnswerList())
+                .build();
+    }
+
+    List<Answer> buildAnswerList() {
+        List<Answer> answerList = new ArrayList<>();
+        answerList.add(Answer.builder().answerCount(0).optionNo(1).option("Option1").build());
+        answerList.add(Answer.builder().answerCount(0).optionNo(2).option("Option2").build());
+        answerList.add(Answer.builder().answerCount(0).optionNo(3).option("Option3").build());
+        answerList.add(Answer.builder().answerCount(0).optionNo(4).option("Option4").build());
+        return answerList;
     }
 }
